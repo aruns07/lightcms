5df0f491036a1db2648f5416f65a51fd28f9448e
Arun Saini <Arun.Saini@akqa.com>
2016-07-20 12:02:11 +0530

    contentful api

--- web.js
+++ web.js
@@ -1,13 +1,26 @@
+'use strict';
+
+
 let express = require('express');
 let app = express();
 
+let contentful = require('contentful');
+let client = contentful.createClient({
+	space: 'xbpa6t4nntz2',
+	accessToken: 'e1110ae644b75e50373c2162faa6f082d543b87763fdc303974ae020ee37ce5a'
+});
+
 app.use(express.static('.'));
 
-app.get('/carousel', function(req, res){
-	res.send('carousel data');
+app.get('/carousel', (req, res) => {
+	client.getEntry('1NEONRexcIIYis6GKkqMw6')
+		.then((entry) => {
+			res.send('carousel data : ' + JSON.stringify(entry));		
+		});
+	
 });
 
 let port = process.env.PORT || 3000;
-app.listen(port, function() {
+app.listen(port, () => {
 	console.log('App listening on port '+ port);
 });
